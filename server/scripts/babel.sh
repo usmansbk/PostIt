c76e37deb112a77d1a49359430fc37ade040b39c
usmansbk <usmansbk@gmail.com>
2017-12-27 00:30:28 +0100

    Add script

---
+++ server/scripts/babel.sh
@@ -0,0 +1,5 @@
+babel server.js --out-file build/server.js
+babel controllers --out-dir build/controllers
+babel middlewares --out-dir build/middlewares
+babel helpers --out-dir build/helpers
+babel tests --out-dir build/tests
