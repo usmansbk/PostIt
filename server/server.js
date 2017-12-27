206a099cdc9b0fafbf0414f8c465444832aef29c
usmansbk <usmansbk@gmail.com>
2017-12-27 02:56:21 +0100

    Add server index route

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import Logger from 'bunyan';
+import apiRouter from './routes/api';
 
 const app = express();
 const port = process.env.PORT || 8888;
 const router = express.Router();
 const log = new Logger({name: 'Server'});
+const appInfo = {
+  "name": 'PostIt Server',
+  "version": '1.0.0',
+  "status": 'online',
+  "about": 'This application allows people create accounts, create groups and add registerd users to the groups, and the send messages out to these groups whenever they want.',
+  "author": 'Babakolo Usman Suleiman',
+  "contact": 'https://github.com/usmansbk',
+};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-router.get('/', (req, res) =>
-  res.json({ message: 'hooray! welcome to our api!'}));
+router.get('/', (req, res) => {
+  res.json(appInfo);
+});
+
+router.use('/api', apiRouter);
 
 app.use('/', router);
 
