599247c14d17c0015fee834e9b6b260f515b93d9
usmansbk <usmansbk@gmail.com>
2017-12-27 15:20:36 +0100

    Change app entry point

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import Logger from 'bunyan';
 import apiRouter from './routes/api';
 
 const app = express();
-const port = process.env.PORT || 8888;
 const router = express.Router();
-const log = new Logger({ name: 'Server' });
 const appInfo = {
   name: 'PostIt Server',
   version: '1.0.0',
@@ -24,8 +21,6 @@ router.get('/', (req, res) => {
 });
 
 router.use('/api', apiRouter);
-
 app.use('/', router);
 
-app.listen(port);
-log.info(`Server listening to port ${port}`);
+export default app;
