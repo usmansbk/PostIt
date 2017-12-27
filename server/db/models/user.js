9a06e713b8cb977680a0f9aa3265dd338ffda8cb
usmansbk <usmansbk@gmail.com>
2017-12-27 20:58:52 +0100

    Create user model

---
+++ server/db/models/user.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    firstname: DataTypes.STRING,
+    surname: DataTypes.STRING,
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    birthday: DataTypes.STRING,
+    gender: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
