62f4f4dcc5eaff4246dcd0d963c814e504378341
usmansbk <usmansbk@gmail.com>
2017-12-28 00:21:15 +0100

    Transpile database files

--- server/scripts/babel.sh
+++ server/scripts/babel.sh
@@ -5,3 +5,9 @@ babel middlewares --out-dir build/middlewares
 babel helpers --out-dir build/helpers
 babel tests --out-dir build/tests
 babel routes --out-dir build/routes
+
+babel db/models --out-dir build/db/models
+babel db/migrations --out-dir build/db/migrations
+babel db/seeders --out-dir build/db/seeders
+babel db/config/config.js --out-file build/db/config/config.js
+
