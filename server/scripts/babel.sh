2ea6407c2e4eef63c9a20ee054736e19a3c249da
usmansbk <usmansbk@gmail.com>
2017-12-27 02:56:58 +0100

    Add routes transpile command

--- server/scripts/babel.sh
+++ server/scripts/babel.sh
@@ -3,3 +3,4 @@ babel controllers --out-dir build/controllers
 babel middlewares --out-dir build/middlewares
 babel helpers --out-dir build/helpers
 babel tests --out-dir build/tests
+babel routes --out-dir build/routes
