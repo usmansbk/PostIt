5df15467b9d4d1d485fcff23c6de2c8d76c3c421
usmansbk <usmansbk@gmail.com>
2017-12-27 15:18:49 +0100

    Add app entry point transpile command

--- server/scripts/babel.sh
+++ server/scripts/babel.sh
@@ -1,4 +1,5 @@
 babel server.js --out-file build/server.js
+babel bin/www --out-file build/bin/www
 babel controllers --out-dir build/controllers
 babel middlewares --out-dir build/middlewares
 babel helpers --out-dir build/helpers
