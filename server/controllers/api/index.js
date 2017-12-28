10ac928e85c1a59860c5796b55e19889c80f99d5
usmansbk <usmansbk@gmail.com>
2017-12-29 00:30:14 +0100

    Add stub controller

---
+++ server/controllers/api/index.js
@@ -0,0 +1,4 @@
+import User from './user';
+//import Group from './group/group';
+
+export { User, /*Group*/ };
---
+++ server/controllers/api/user.js
@@ -0,0 +1,10 @@
+export default class User {
+
+  static signin(req, res) {
+    res.send('class works!');
+  }
+
+  static signup(req, res) {
+    res.send('class works!');
+  }
+}
