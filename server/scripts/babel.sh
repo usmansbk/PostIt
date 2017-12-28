c5ca21df67ee9bd32175355bdb0d478a35d21848
usmansbk <usmansbk@gmail.com>
2017-12-28 02:54:23 +0100

    Transpile migrations and seeders

--- server/scripts/babel.sh
+++ server/scripts/babel.sh
@@ -1,4 +1,3 @@
-babel ._sequelizerc --out-file .sequelizerc
 babel server.js --out-file build/server.js
 babel bin/www --out-file build/bin/www
 babel controllers --out-dir build/controllers
@@ -10,5 +9,3 @@ babel routes --out-dir build/routes
 babel db/models --out-dir build/db/models
 babel db/migrations --out-dir build/db/migrations
 babel db/seeders --out-dir build/db/seeders
-babel db/config/config.js --out-file build/db/config/config.js
-
