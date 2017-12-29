1f01ac8c4a87b855bfb78f2df7cdf2ec7d58f9a7
usmansbk <usmansbk@gmail.com>
2017-12-29 15:38:10 +0100

    Add user controller

--- server/controllers/api/user.js
+++ server/controllers/api/user.js
@@ -3,13 +3,39 @@ import { User } from '../../../db/models';
 export default class UserController {
 
   static signin(req, res) {
-    const { username, password } = req.body;
-    res.send('class works!');
+    const body = req.body;
+    User.findOne({ where: body }).then(user => {
+      if (user) {
+        res.status(200).json({
+          status: 'success',
+          data: user
+        });
+      } else {
+        res.status(400).json({
+          status: 'fail',
+          message: 'Invalid username or password',
+          data: user
+        });
+      }
+    }).catch(error => {
+      res.status(500).json({
+        status: 'error',
+        message: 'Database error'
+      });
+    });
   }
 
   static signup(req, res) {
     User.create(req.body).then(user => {
-      res.status(201);
+      res.status(201).json({
+        status: 'success',
+        data: user
+      });
+    }).catch(error => {
+      res.status(400).json({
+        status: 'fail',
+        data: error
+      });
     });
   }
 }
