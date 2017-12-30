aba7d9c15d12c88269ce0947b1661bfa6d0b777f
usmansbk <usmansbk@gmail.com>
2017-12-30 16:51:18 +0100

    Rename method

--- server/routes/api/group/index.js
+++ server/routes/api/group/index.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import user from './user';
-import message from './message';
-import messages from './messages';
 import { GroupController } from '../../../controllers/api';
 
 const router = express.Router();
 
-router.use('/:guid/user', GroupController.addUser);
+router.use('/:guid/user', GroupController.addUsers);
 router.use('/:guid/message', GroupController.postMessage);
 router.use('/:guid/messages', GroupController.retrieveMessages);
 router.post('/', GroupController.createGroup);
