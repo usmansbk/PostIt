a6d33914686dad5f69bb57ef213e79bb1e1438cd
usmansbk <usmansbk@gmail.com>
2017-12-30 20:26:59 +0100

    Dont transpile spec

--- server/scripts/babel.sh
+++ server/scripts/babel.sh
@@ -3,5 +3,4 @@ babel bin/www --out-file build/bin/www
 babel controllers --out-dir build/controllers
 babel middlewares --out-dir build/middlewares
 babel helpers --out-dir build/helpers
-babel tests --out-dir build/tests
 babel routes --out-dir build/routes
