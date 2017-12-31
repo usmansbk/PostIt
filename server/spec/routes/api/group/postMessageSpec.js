eebccd106206cb9a0eb2c0ef86b61bd54936b20d
usmansbk <usmansbk@gmail.com>
2017-12-31 16:35:12 +0100

    Add spec stub

---
+++ server/spec/routes/api/group/postMessageSpec.js
@@ -0,0 +1,35 @@
+const request = require('request'),
+  baseUrl = 'http://localhost:8888/api/group/',
+  endPoint = '/message';
+
+describe('POST:/api/group/<group id>/message', () => {
+  describe('API route that allows a logged in user post messages to created groups', () => {
+
+    describe('Making a GET request to route', () => {
+      it('should return status code 405', (done) => {
+        request.get(`{baseUrl}1${endPoint}`, (err, res, body) => {
+          expect(res.statusCode).toBe(405);
+          done();
+        });
+      });
+    });
+
+    describe('Unauthenticated access to route', () => {
+
+    });
+
+    describe('Authenticated submission of form with', () => {
+
+      describe('null message field', () => {
+      });
+
+      describe('empty string message', () => {
+      });
+
+      describe('with message to a group user in not a member of', () => {
+      });
+
+    });
+
+  });
+});
