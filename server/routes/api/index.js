49d47986091313e89e32e4cfd46e03f7e3c3e713
usmansbk <usmansbk@gmail.com>
2017-12-31 14:11:59 +0100

    Handler unsupported methods

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -6,5 +6,12 @@ const router = express.Router();
 
 router.use('/user', user);
 router.use('/group', group);
+router.all('*', (req, res) => {
+  res.status(405).json({
+    status: 'fail',
+    message: 'Method not supported',
+    data: null
+  });
+});
 
 export default router;
