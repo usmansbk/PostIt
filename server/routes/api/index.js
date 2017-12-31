20b45014b5f2dc3aebf2fa8fc65b526a6dc12c91
usmansbk <usmansbk@gmail.com>
2017-12-31 22:54:04 +0100

    Add authentication middleware

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import user from './user';
 import group from './group';
+import { Route } from '../../middlewares';
 
 const router = express.Router();
 
 router.use('/user', user);
-router.use('/group', group);
+router.use('/group', Route.auth, group);
 router.all('*', (req, res) => {
   res.status(405).json({
     status: 'fail',
