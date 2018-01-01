4b3769bced23c0ec141c998e12929de4169a97ef
usmansbk <usmansbk@gmail.com>
2018-01-01 18:09:26 +0100

    Validate route form data

---
+++ server/src/middlewares/validate.js
@@ -0,0 +1,51 @@
+import validator from 'validator';
+
+const isEmpty = /^\s*$/m,
+  MAX_NAME_LEN = 22,
+  MAX_PURPOSE_LENGTH = 50;
+
+export default class Validate {
+  static createGroup(req, res, next) {
+    const { name, purpose } = req.body;
+    if (!name || isEmpty.test(name) ||
+        name.length > MAX_NAME_LEN ||
+        (purpose && purpose.length > MAX_PURPOSE_LENGTH) ) {
+      res.status(400).json({
+        status: 'fail',
+        data: {
+          message: 'name: name is required'
+        }
+      });
+      return;
+    }
+    next();
+  }
+
+  static addUser(req, res, next) {
+    const { invites } = req.body;
+    if (!invites || isEmpty.test(invites)) {
+      res.status(400).json({
+        status: 'fail',
+        data: {
+          message: 'invites: A list of users is required'
+        }
+      });
+      return;
+    }
+    next();
+  }
+
+  static postMessage(req, res, next) {
+    const { message } = req.body;
+    if (!message || isEmpty.test(message)) {
+      res.status(400).json({
+        status: 'fail',
+        data: {
+          message: 'message: A message is required'
+        }
+      });
+      return;
+    }
+    next();
+  }
+}
