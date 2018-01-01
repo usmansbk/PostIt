282ab459058cc74d780e02cbf3a66b6210f5ab96
usmansbk <usmansbk@gmail.com>
2018-01-01 06:24:57 +0100

    Fix grammer

--- server/src/server.js
+++ server/src/server.js
@@ -29,5 +29,13 @@ app.get('/', (req, res) => {
 });
 
 app.use(router);
+app.use((err, req, res, next) => {
+  res.status(500).
+    json({
+      status: 'error',
+      message: 'Nothing you can do about it!'
+    });
+  return;
+});
 
 export default app;
