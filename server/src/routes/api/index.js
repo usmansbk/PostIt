706c814f48760baa7810bf75042b8123d903f831
usmansbk <usmansbk@gmail.com>
2018-01-01 17:54:21 +0100

    Fix validation middlewares

--- server/src/routes/api/index.js
+++ server/src/routes/api/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Route } from '../../middlewares';
+import { Route, Validate } from '../../middlewares';
 import { GroupController, UserController } from '../../controllers';
 
 const router = express.Router();
@@ -8,9 +8,9 @@ router.use('/api/group/', Route.isAuthenticated);
 
 router.post('/api/user/signup', UserController.signUp);
 router.post('/api/user/signin', UserController.signIn);
-router.post('/api/group/', GroupController.createGroup);
-router.post('/api/group/:guid/message', GroupController.postMessage);
-router.post('/api/group/:guid/user', GroupController.addUsers);
+router.post('/api/group/', Validate.createGroup, GroupController.createGroup);
+router.post('/api/group/:guid/message', Validate.postMessage, GroupController.postMessage);
+router.post('/api/group/:guid/user', Validate.addUser, GroupController.addUsers);
 router.get('/api/group/:guid/messages', GroupController.retrieveMessages);
 
 export default router;
