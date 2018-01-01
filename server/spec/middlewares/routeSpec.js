bf9eeda1801a0ae94d7437fcc1b2ac6999ef13a6
usmansbk <usmansbk@gmail.com>
2018-01-01 23:34:40 +0100

    Write test

---
+++ server/spec/middlewares/routeSpec.js
@@ -0,0 +1,41 @@
+const middlewares = require('../../build/middlewares');
+
+const { Route } = middlewares,
+  request = {
+    session: {}
+  },
+  response = {
+    status: function status(code) {
+      this.statusCode = code;
+      return this;
+    },
+
+    json: function(obj) {
+    }
+  };
+
+let callback = function callback() {
+  response.statusCode = 200;
+};
+
+describe('The isAuthenticated function would grant access.', () => {
+  describe('When request session property doesn\'t contains userId property', () => {
+    it('should return a status code of 403', () => {
+      request.session.userId = false;
+      Route.isAuthenticated(request, response, callback);
+      expect(response.statusCode).toBe(403);
+    });
+  });
+
+  describe('When request session property contains userId property', () => {
+    describe('the callback function is invoked and', () => {
+      Route.isAuthenticated(request, response, callback);
+      it('should set the value of response status to 200', () => {
+        request.session.userId = true;
+        Route.isAuthenticated(request, response, callback);
+        expect(response.statusCode).toBe(200);
+      });
+    });
+  });
+});
+
