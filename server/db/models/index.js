5914eee3ad3d0abe7e8779bcb3e3951c3a9af1b6
usmansbk <usmansbk@gmail.com>
2018-01-03 16:53:21 +0100

    Cleaning up

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(path.resolve(__dirname, '../config/config.js'))[env];
+const config = require('../config/config.js')[env];
+
 const db = {};
 
 let sequelize;
--- server/scripts/build.sh
+++ server/scripts/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+
+## Make required build directories
 if test -d ./server/build;
 then
   rm -r server/build
@@ -7,12 +9,21 @@ mkdir server/build
 cd server/build
 mkdir bin controllers helpers middlewares routes
 mkdir routes/api
-
 cd ../../
 
+## Transpile src/ codes to build/
 ./node_modules/.bin/babel server/src/server.js --out-file server/build/server.js
 ./node_modules/.bin/babel server/src/bin/www --out-file server/build/bin/www
 ./node_modules/.bin/babel server/src/controllers --out-dir server/build/controllers
 ./node_modules/.bin/babel server/src/middlewares --out-dir server/build/middlewares
 ./node_modules/.bin/babel server/src/helpers --out-dir server/build/helpers
 ./node_modules/.bin/babel server/src/routes --out-dir server/build/routes
+
+## Set environment variables for development
+export NODE_ENV=development
+export DEV_DB_USERNAME=usman
+export DEV_DB_PASSWORD=null
+export DEV_DB_NAME=postit_dev
+export DEV_DB_HOSTNAME='127.0.0.1'
+export DEV_DB_PORT=5432
+export DIALECT=postgres
