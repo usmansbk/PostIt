8c9eafbd917f90b232f2a2df0b6e333ad361fab5
usmansbk <usmansbk@gmail.com>
2018-01-04 05:17:49 +0100

    Use heroku postgresql

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -17,11 +17,6 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_URL'
   }
 };
