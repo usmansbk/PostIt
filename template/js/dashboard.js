fdcaee77e2bee6a03890560855a2f54a824c5392
usmansbk <usmansbk@gmail.com>
2018-01-06 21:01:34 +0100

    Upgrade templates

--- template/js/dashboard.js
+++ template/js/dashboard.js
@@ -1,54 +1,25 @@
 (function () {
-  const module = {};
+  let newPostContainer = document.querySelector('#newpost-container')
+    , newGroupContainer = document.querySelector('#newgroup-container')
+    , dashboard = document.querySelector('#dashboard');
 
-  let newpostBtn
-    , newgroupBtn
-    , cancelCreateGroupBtn
-    , cacelCreatePostBtn
-    , postFormBtn
-    , groupFormBtn
-    , main
-    , newPostNodes
-    , newGroupNodes;
+  document.addEventListener('click', (event) => {
+    let target = event.target
+      , value = target.value;
 
-  newpostBtn = document.querySelector('#newpost-btn');
-  newgroupBtn = document.querySelector('#newgroup-btn');
-  cancelCreateGroupBtn = document.querySelector('#cancel-create-group');
-  cancelCreatePostBtn = document.querySelector('#cancel-create-post');
-  postFormBtn = document.querySelector('#post-form-btn');
-  groupFormBtn = document.querySelector('#group-form-btn');
-  newPostNodes = document.querySelectorAll('[form=newpost]');
-  newGroupNodes = document.querySelectorAll('[form=newgroup]');
-  dashboard = document.querySelector('#dashboard');
-  newgroupForm = document.querySelector('.newgroup');
-  newpostForm = document.querySelector('.newpost');
+    if (value === 'Post') {
+      dashboard.style.display = 'none';
+      newPostContainer.style.display = 'block';
+    } else if (value === '+') {
+      dashboard.style.display = 'none';
+      newGroupContainer.style.display = 'block';
+    } else if (value == 'Cancel') {
+      newPostContainer.style.display = 'none';
+      newGroupContainer.style.display = 'none';
+      dashboard.style.display = 'block';
+    }});
 
-  const formAction = function cancelAction(event) {
-    const target = event.target;
-    let className = target.getAttribute('for');
-    const container = document.querySelector(`.${className}`);
-    const value = target.value.toLowerCase();
-
-    containerDisplay = 'none';
-    dashboardDisplay = 'block'
-    if (value !== 'cancel') {
-      containerDisplay = 'block',
-      dashboardDisplay = 'none';
-    }
-    container.style.display = containerDisplay;
-    dashboard.style.display = dashboardDisplay;
-  };
-
-  newgroupForm.style.display = 'none';
-  newpostForm.style.display = 'none';
-  cancelCreatePostBtn.addEventListener('click', formAction);
-  cancelCreateGroupBtn.addEventListener('click', formAction);
-  postFormBtn.addEventListener('click', formAction);
-  groupFormBtn.addEventListener('click', formAction);
-  newgroupBtn.addEventListener('click', (event) => {
-    console.log('Group created');
-  });
-  newpostBtn.addEventListener('click', (event) => {
-    console.log('Message posted');
-  });
+  newPostContainer.style.display = 'none';
+  newGroupContainer.style.display = 'none';
+  dashboard.style.display = 'block';
 })();
