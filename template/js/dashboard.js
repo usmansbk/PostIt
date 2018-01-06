77efc35af2ffaa5e6427119f2162a0adb16d5264
usmansbk <usmansbk@gmail.com>
2018-01-06 23:35:12 +0100

    Improve templates

--- template/js/dashboard.js
+++ template/js/dashboard.js
@@ -1,7 +1,62 @@
 (function () {
   let newPostContainer = document.querySelector('#newpost-container')
     , newGroupContainer = document.querySelector('#newgroup-container')
-    , dashboard = document.querySelector('#dashboard');
+    , dashboard = document.querySelector('#dashboard')
+    , createGroupBtn = document.querySelector('#create-group-btn')
+    , createPostBtn = document.querySelector('#create-post-btn')
+    , board = document.querySelector('#board')
+    , grouplist = document.querySelector('#grouplist')
+    , textarea = document.querySelector('textarea');
+
+    textarea.value = '';
+
+  let createCard = function createCard(message) {
+    let card = document.createElement('div');
+    card.setAttribute('class', 'card');
+    let cardBody = document.createElement('div');
+    cardBody.setAttribute('class', 'card-body');
+    let cardText = document.createElement('p');
+    cardText.innerText = message;
+    let footer = document.createElement('p');
+    let time = document.createElement('small');
+    time.setAttribute('class', 'text-muted');
+    time.innerText = 'Posted on ' + (new Date().toString());
+
+    cardBody.append(cardText);
+    cardBody.append(time);
+    card.append(cardBody);
+    return card;
+  };
+
+  let createGroup = function createGroup(name, purpose) {
+    let item = document.createElement('a');
+    item.setAttribute('class', 'list-group-item list-group-item-action d-flex justify-content-between-align-items-center');
+    item.setAttribute('href', '#');
+    item.innerText = name;
+    return item;
+  };
+
+  createPostBtn.addEventListener('click', (event) => {
+    let value = textarea.value;
+    textarea.value = '';
+    let card = createCard(value);
+    board.append(card);
+    newPostContainer.style.display = 'none';
+    dashboard.style.display = 'block';
+    event.preventDefault();
+    event.stopPropagation();
+  });
+
+  createGroupBtn.addEventListener('click', (event) => {
+    let name = document.querySelector('#group-name');
+    name = name.value;
+    let item = createGroup(name, null);
+    grouplist.append(item);
+    newGroupContainer.style.display = 'none';
+    dashboard.style.display = 'block';
+    event.preventDefault();
+    event.stopPropagation();
+  });
 
   document.addEventListener('click', (event) => {
     let target = event.target
