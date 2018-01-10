f49b6d82199b87d3a1b289692440541c6ce2c6ae
usmansbk <usmansbk@gmail.com>
2018-01-10 20:36:53 +0100

    Update server

--- server/src/helpers/info.js
+++ server/src/helpers/info.js
@@ -1,6 +1,6 @@
 export default {
   name: 'PostIt Server',
-  version: '1.0.0',
+  version: '1.1.0',
   status: 'online',
   about: 'This application allows people create accounts, create groups and add registerd users to the groups, and the send messages out to these groups whenever they want.',
   author: 'Babakolo Usman Suleiman',
