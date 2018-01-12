68218a1a88c91bc22eb03522d10caa73810a7e26
usmansbk <usmansbk@gmail.com>
2018-01-12 17:00:46 +0100

    Configure webpack for tranpile

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,34 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './client/js/source/index.js',
+  entry: [
+    './client/js/source/index.js',
+    './client/stylesheets/sass/index.scss'
+  ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'client/js')
-  }
+    path: path.resolve(__dirname, 'js')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('../stylesheets/style.css')
+  ]
 };
