090523ab33f048f42797cf45fceb85e97d632d1a
usmansbk <usmansbk@gmail.com>
2018-01-14 22:26:52 +0100

    Add form validation

--- client/js/source/components/Sign/SignUpForm.jsx
+++ client/js/source/components/Sign/SignUpForm.jsx
@@ -2,23 +2,48 @@ import React from 'react';
 import InputField from '../common/InputField.jsx';
 import Button from '../common/Button.jsx';
 
-export default function SignUpForm(props) {
-  return (
+export default class SignUpForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.validatePassword = this.validatePassword.bind(this);
+    this.state = { password: '', confirm: '', isInvalid: false };
+  }
+
+  validatePassword(event) {
+    const value = event.target.value;
+    const id = event.target.id;
+    this.setState({
+      [id]: value
+    }, () => {
+      if (this.state.confirm !== this.state.password) {
+        this.setState({isInvalid: true});
+        return;
+      }
+      this.setState({isInvalid: false});
+    });
+  }
+
+  render() {
+   const error = <p className='red-text'>Password doesn't match</p>
+    return (
       <form>
 
-        <InputField id='fusername' type='text' required='true' label='Username' className='input-field'>
+        <InputField id='username' type='text' required='true' label='Username' className='input-field'>
           Name can only contain letters, numbers and underscore
         </InputField>
 
-        <InputField id='femail' type='email' required='true' label='Email address' className='input-field'/>
+        <InputField id='email' type='email' required='true' label='Email address' className='input-field'/>
         
-        <InputField id='fpassword' type='password' required='true' label='Password' className='input-field'>
+        <InputField id='password' type='password' required='true' value={this.state.password} label='Password' validate='validate' className='input-field' onChange={this.validatePassword} >
           Password must be 8-32 characters long
         </InputField>
         
-        <InputField id='fconfirm' type='password' required='true' label='Confirm password' className='input-field' />
+        <InputField id='confirm' type='password' required='true' label='Confirm password' className='input-field' onChange={this.validatePassword} value={this.state.fconfirm} />
+        { this.state.isInvalid && error }
         <Button type='submit' className='center-align' value='Sign Up'>send</Button>
       
      </form>
-  )
+    );
+  }
 }
--- client/js/source/components/common/InputField.jsx
+++ client/js/source/components/common/InputField.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 export default function InputField(props) {
   return (<div className={props.className} >
     <label htmlFor={props.id}>{props.label}</label>
-    <input id={props.id} type={props.type} name={props.name} value={props.value} required={props.required}/>
+    <input className={props.validate} id={props.id} type={props.type} name={props.name} value={props.value} required={props.required} onChange={props.onChange}/>
     {
       props.children && <span className='helper-text'>{props.children}</span> 
     }
