24351989dc52edb494a298f77ccaa0348a5ea935
usmansbk <usmansbk@gmail.com>
2018-02-03 07:54:25 +0100

    Config for production

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,6 +35,12 @@ module.exports = {
     extensions: ['.js', '.jsx']
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
   ]
 };
